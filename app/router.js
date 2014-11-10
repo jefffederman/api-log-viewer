625b6684dbd186a5b214f804c81e5f63a59ebd4d
Jeff Federman <jefffederman@gmail.com>
2014-11-10 15:17:03 -0500

    Add events show

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('events');
+  this.route('events', function() {
+    this.route('show', { path: '/:id' });
+  });
 });
 
 export default Router;
